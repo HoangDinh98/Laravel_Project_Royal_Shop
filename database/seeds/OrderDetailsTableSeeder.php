9c2ff7694bf75378def9bdf492b3f7cdbc62c6c6
PN\sinh.truong <sinh.truong@student.passerellesnumeriques.org>
2018-04-18 17:20:24 +0700

    dB seeder

--- database/seeds/OrderDetailsTableSeeder.php
+++ database/seeds/OrderDetailsTableSeeder.php
@@ -14,8 +14,29 @@ class OrderDetailsTableSeeder extends Seeder {
         DB::table('order_details')->insert(
                 array(
                     ['order_id' => 1, 'product_id' => 1, 'quantity' => 2, 'original_price' => 1250000, 'price' => 1250000, 'created_at' => $times, 'updated_at' => $times],
-                    ['order_id' => 1, 'product_id' => 3, 'quantity' => 1, 'original_price' => 350000, 'price' => 30000000, 'created_at' => $times, 'updated_at' => $times],
-                    ['order_id' => 2, 'product_id' => 5, 'quantity' => 3, 'original_price' => 1250000, 'price' => 1250000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 1, 'product_id' => 3, 'quantity' => 1, 'original_price' => 35000000, 'price' => 35000000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 2, 'product_id' => 5, 'quantity' => 3, 'original_price' => 12000000, 'price' => 12000000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 2, 'product_id' => 7, 'quantity' => 2, 'original_price' => 500000, 'price' => 500000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 3, 'product_id' => 8, 'quantity' => 3, 'original_price' => 1000000, 'price' => 1000000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 3, 'product_id' => 9, 'quantity' => 3, 'original_price' => 1000000, 'price' => 1000000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 4, 'product_id' => 10, 'quantity' => 4, 'original_price' => 650000, 'price' => 650000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 4, 'product_id' => 6, 'quantity' => 5, 'original_price' => 8000000, 'price' => 8000000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 5, 'product_id' => 3, 'quantity' => 2, 'original_price' => 1200000, 'price' => 1200000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 5, 'product_id' => 2, 'quantity' => 3, 'original_price' => 950000, 'price' => 950000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 6, 'product_id' => 7, 'quantity' => 4, 'original_price' => 800000, 'price' => 800000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 6, 'product_id' => 8, 'quantity' => 5, 'original_price' => 900000, 'price' => 900000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 7, 'product_id' => 9, 'quantity' => 6, 'original_price' => 700000, 'price' => 700000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 7, 'product_id' => 1, 'quantity' => 7, 'original_price' => 55000000, 'price' => 55000000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 8, 'product_id' => 4, 'quantity' => 3, 'original_price' => 5000000, 'price' => 5000000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 8, 'product_id' => 5, 'quantity' => 5, 'original_price' => 4500000, 'price' => 4500000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 9, 'product_id' => 6, 'quantity' => 2, 'original_price' => 6000000, 'price' => 6000000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 9, 'product_id' => 7, 'quantity' => 3, 'original_price' => 3999000, 'price' => 3999000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 10, 'product_id' => 8, 'quantity' => 3, 'original_price' => 4999000, 'price' => 4999000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 10, 'product_id' => 9, 'quantity' => 3, 'original_price' => 3999000, 'price' => 3999000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 11, 'product_id' => 5, 'quantity' => 3, 'original_price' => 7999000, 'price' => 7999000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 11, 'product_id' => 1, 'quantity' => 3, 'original_price' => 4999000, 'price' => 4999000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 12, 'product_id' => 3, 'quantity' => 3, 'original_price' => 6999000, 'price' => 6999000, 'created_at' => $times, 'updated_at' => $times],
+                    ['order_id' => 12, 'product_id' => 6, 'quantity' => 3, 'original_price' => 9999000, 'price' => 9999000, 'created_at' => $times, 'updated_at' => $times],
                 )
         );
     }
--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -15,7 +15,19 @@ class OrdersTableSeeder extends Seeder {
                 array(
                     ['customer_name' => 'Nguyễn Văn Hải', 'phone' => '0169877899', 'email' => 'hai123@gmail.com', 'address' => '130 Lý Thường Kiệt, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
                     ['customer_name' => 'Trần Mai Phương', 'phone' => '0868234567', 'email' => 'phuong123@gmail.com', 'address' => '72 Nguyễn Văn Linh, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
-                )
+                    ['customer_name' => 'Nguyễn Văn Hải', 'phone' => '0169877899', 'email' => 'hai98@gmail.com', 'address' => '101 Lê Hữu Trác, Sơn Trà, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Đinh Thanh Hoàng', 'phone' => '0168393838', 'email' => 'hoangdth98@gmail.com', 'address' => '99 Nguyễn Du, Hòa Cường Bắc, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Nguyễn Thị Kim Xuyến', 'phone' => '0169877888', 'email' => 'Xuyen8080@gmail.com', 'address' => '28 ĐỐng Đa, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Hồ Thị Ngọc Anh', 'phone' => '0169777999', 'email' => 'anh.ho@gmail.com', 'address' => '08 Nguyễn Thị mInh Khai, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Trương Dương Trường Sinh', 'phone' => '0169777899', 'email' => 'sinh.truong985@gmail.com', 'address' => '130 Lý Nhân Tông, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Nguyễn THị Tràng Hải', 'phone' => '0162887709', 'email' => 'hai.trang@gmail.com', 'address' => '24 Hùng Vương, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Nguyễn Thị Xuyến Xao', 'phone' => '0163875809', 'email' => 'xaoxuyen@gmail.com', 'address' => '99 Hồ Nghinh, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Lê THị Kim Quyên', 'phone' => '0165877800', 'email' => 'quyen@gmail.com', 'address' => '15 Lý Thường Kiệt, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Nguyễn Xuân Bảo', 'phone' => '0165693023', 'email' => 'xuanbao@gmail.com', 'address' => '90 Hồ Nghinh, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+                    ['customer_name' => 'Lê Hoàng Anh', 'phone' => '01687074190', 'email' => 'hoanganh@gmail.com', 'address' => '100 Lý Bạch, Hải Châu, Đà Nẵng', 'status' => 0, 'description' => 'Thành viên', 'created_at' => $times, 'updated_at' => $times],
+               
+                    
+                    )
         );
     }
 
